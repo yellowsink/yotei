0fb316bfa5dc6ebdb6a69fe0ea077c687484e997
yellowsink <yellowsink@riseup.net>
2022-12-04 20:47:24 +0000

    unfinished task serialisation

--- daemon/source/tasks.d
+++ daemon/source/tasks.d
@@ -8,64 +8,6 @@ enum ScheuleRule
   always
 }
 
-private ScheuleRule parseScheduleRule(string str) @safe
-{
-  final switch (str)
-  {
-  case "drop":
-    return ScheuleRule.drop;
-  case "sigle":
-    return ScheuleRule.single;
-  case "always":
-    return ScheuleRule.always;
-  }
-}
-
-private int intervalToMultiplier(string interval) @safe
-{
-  final switch (interval)
-  {
-  case "seconds":
-    return 1000;
-  case "minutes":
-    return 1000 * 60;
-  case "hours":
-    return 1000 * 60 * 60;
-  case "days":
-    return 1000 * 60 * 60 * 24;
-  case "weeks":
-    return 1000 * 60 * 60 * 24 * 7;
-    // ah huh. uh. 30? 31?
-    // TODO
-  case "months":
-    return 1000 * 60 * 60 * 30;
-    // leap years?
-  case "years":
-    return 1000 * 60 * 60 * 365;
-  }
-}
-
-private Duration resolveInterval(string interval) @safe
-{
-  import std.array : split;
-
-  auto splits = interval.split(" ");
-
-  auto ms = 0;
-
-  for (auto i = 0; i + 1 < splits.length; i += 2)
-  {
-    import std.conv : to;
-
-    auto amount = to!double(splits[i]);
-    auto multiplier = intervalToMultiplier(splits[i + 1]);
-
-    ms += to!int(amount * multiplier);
-  }
-
-  return dur!"msecs"(ms);
-}
-
 struct Task
 {
   import std.typecons : Nullable;
@@ -81,9 +23,14 @@ struct Task
   Nullable!Date once;
   Nullable!Duration at;
 
+  // keep this as D:YAML will mimick the ingested code style on emit
+  private Nullable!Node yamlRepresentation;
+
   // yaml
   this(const Node node, string tag) @safe
   {
+    yamlRepresentation = node;
+
     id = node["id"].as!string;
     run = node["run"].as!string;
     as = node["as"].as!string;
@@ -108,69 +55,167 @@ struct Task
     if ("at" in node)
       at = dur!"minutes"(node["at"].as!int);
   }
-}
 
-private static struct SysTimePackProxy
-{
-  import msgpack : Packer, Unpacker;
-
-  static void serialize(ref Packer p, ref in SysTime tim)
+  Node toYaml() @safe
   {
-    p.pack(tim.toISOExtString());
-  }
+    Node node = yamlRepresentation.get(Node(new Node.Pair[0]));
+    
+    node["id"] = id;
+    node["run"] = run;
 
-  static void deserialize(ref Unpacker u, ref SysTime tim)
-  {
-    string tmp;
-    u.unpack(tmp);
-    tim = SysTime.fromISOExtString(tmp);
-  }
-}
+    // schedule rule might have been left as default!
+    if (!("scheduleRule" in node) || scheduleRule != ScheuleRule.single)
+      node["scheduleRule"] = emitScheduleRule(scheduleRule);
 
-struct TaskInternals
-{
-  import msgpack : nonPacked, serializedAs;
+    if (!condition.isNull)
+      node["condition"] = condition.get();
+    else if ("condition" in node)
+      node.removeAt("condition");
+    
+    //if ()
 
-  @nonPacked string id;
-  @serializedAs!SysTimePackProxy SysTime last;
-
-  this(SysTime last)
-  {
-    import std.datetime : Clock;
-
-    this.last = last;
+    return node;
   }
 }
 
-Task[] loadTasks()
+private
 {
-  import std.file : exists;
-  import dyaml : Loader;
-  import std.algorithm : map;
+  @safe
+  {
+    ScheuleRule parseScheduleRule(string str)
+    {
+      final switch (str)
+      {
+      case "drop":
+        return ScheuleRule.drop;
+      case "single":
+        return ScheuleRule.single;
+      case "always":
+        return ScheuleRule.always;
+      }
+    }
 
-  if (!exists("/etc/yotei/tasks"))
-    return [];
+    string emitScheduleRule(ScheuleRule s)
+    {
+      final switch (s)
+      {
+      case ScheuleRule.drop:
+        return "drop";
+      case ScheuleRule.single:
+        return "single";
+      case ScheuleRule.always:
+        return "always";
+      }
+    }
 
-  auto root = Loader.fromFile("/etc/yotei/tasks").load();
+    int intervalToMultiplier(string interval)
+    {
+      final switch (interval)
+      {
+      case "seconds":
+        return 1000;
+      case "minutes":
+        return 1000 * 60;
+      case "hours":
+        return 1000 * 60 * 60;
+      case "days":
+        return 1000 * 60 * 60 * 24;
+      case "weeks":
+        return 1000 * 60 * 60 * 24 * 7;
+        // ah huh. uh. 30? 31?
+        // TODO
+      case "months":
+        return 1000 * 60 * 60 * 30;
+        // leap years?
+      case "years":
+        return 1000 * 60 * 60 * 365;
+      }
+    }
 
-  auto tasks = new Task[0];
+    Duration resolveInterval(string interval)
+    {
+      import std.array : split;
 
-  foreach (Task task; root)
-    tasks ~= task;
+      auto splits = interval.split(" ");
 
-  return tasks;
-}
+      auto ms = 0;
 
-TaskInternals loadInternals(string id)
-{
-  import std.file : read;
-  import msgpack : unpack;
+      for (auto i = 0; i + 1 < splits.length; i += 2)
+      {
+        import std.conv : to;
 
-  auto raw = cast(ubyte[]) read("/etc/yotei/internal");
+        auto amount = to!double(splits[i]);
+        auto multiplier = intervalToMultiplier(splits[i + 1]);
 
-  auto internals = raw.unpack!(TaskInternals[string]);
+        ms += to!int(amount * multiplier);
+      }
 
-  auto target = internals[id];
-  target.id = id;
-  return target;
+      return dur!"msecs"(ms);
+    }
+  }
+
+  static struct SysTimePackProxy
+  {
+    import msgpack : Packer, Unpacker;
+
+    static void serialize(ref Packer p, ref in SysTime tim)
+    {
+      p.pack(tim.toISOExtString());
+    }
+
+    static void deserialize(ref Unpacker u, ref SysTime tim)
+    {
+      string tmp;
+      u.unpack(tmp);
+      tim = SysTime.fromISOExtString(tmp);
+    }
+  }
+
+  struct TaskInternals
+  {
+    import msgpack : nonPacked, serializedAs;
+
+    @nonPacked string id;
+    @serializedAs!SysTimePackProxy SysTime last;
+
+    this(SysTime last)
+    {
+      import std.datetime : Clock;
+
+      this.last = last;
+    }
+  }
+
+  Task[] loadTasks()
+  {
+    import std.file : exists;
+    import dyaml : Loader;
+    import std.algorithm : map;
+
+    if (!exists("/etc/yotei/tasks"))
+      return [];
+
+    auto root = Loader.fromFile("/etc/yotei/tasks").load();
+
+    auto tasks = new Task[0];
+
+    foreach (Task task; root)
+      tasks ~= task;
+
+    return tasks;
+  }
+
+  TaskInternals loadInternals(string id)
+  {
+    import std.file : read;
+    import msgpack : unpack;
+
+    auto raw = cast(ubyte[]) read("/etc/yotei/internal");
+
+    auto internals = raw.unpack!(TaskInternals[string]);
+
+    auto target = internals[id];
+    target.id = id;
+    return target;
+  }
 }
